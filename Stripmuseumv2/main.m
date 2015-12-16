f8121f8fcaaa3f37df1d88e532cb8b2da16cbb23
Rachouan <rachouan_15@hotmail.fr>
2015-12-16 19:49:33 +0100

    Initial Commit

---
+++ Stripmuseumv2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stripmuseumv2
+//
+//  Created by Rachouan Rejeb on 16/12/15.
+//  Copyright © 2015 Rachouan Rejeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
